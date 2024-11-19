eafcfb09ce8d6505d2bb74871072b7ce923defa4
raj <raj@gmail.com>
2024-11-19 10:13:00 +0545

    Add code for print_value_and_memory_address_and_datatype.py

---
+++ print_value_and_memory_address_and_datatype.py
@@ -0,0 +1,11 @@
+# Define a variable
+x = 42
+
+# Print the value of the variable
+print("Value:", x)
+
+# Print the memory address of the variable using the id() function
+print("Memory Address:", hex(id(x)))
+
+# Print the datatype of the variable using the type() function
+print("Datatype:", type(x))
