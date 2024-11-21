de107524e2a9fcead9ec5fd28cf64568d1b3c174
raj <raj@gmail.com>
2024-11-21 16:05:15 +0545

    variables_and_memory_allocation.py

---
+++ variables_and_memory_allocation.py
@@ -0,0 +1,7 @@
+            ##variabales in python
+#a variable is a reference assigned to the object stored in memory
+# varaible doesnot hold the actual data but it points to the oobject in memory where the data is stored
+
+x=10  #x is called the variable of the object "10"
+print(id(x))
+print(id(10))
