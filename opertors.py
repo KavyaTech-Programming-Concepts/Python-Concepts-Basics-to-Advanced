af3c7e3f4497ca6a92cb7a0f04615971c2ca5d7e
raj <raj@gmail.com>
2024-11-22 10:17:24 +0545

    operators

---
+++ opertors.py
@@ -0,0 +1,36 @@
+#operators are the special symbols or keywords that are used to perfome specific operations on variables and values.
+#there are many types of operators such as:
+
+            ##### arithmetic operators:operates mathematical operations
+#     x+y (addition operator)
+#     x-y (substraction operator)
+#     x*y (multiplication operator)
+#     x/y (division operator)
+#     x%y (modulus or remainder operator)
+#     x**y (power operator)
+
+
+            #####comparison (relationsl) operators
+# x==y (equal to)
+# x!=y (not equals to)
+# x>y   (greater than)
+# x<y     (lesser than)
+# x>=y    (greater than or equals to)
+# x<= y   (lesser than or equals to)
+
+                ######logical operatos
+# (5 > 3) and (3 < 8) → True  (and , Logical AND operator)
+# (5 > 3) or (3 > 8) → True (or , Logical OR operator)
+#  not (5 > 3) → False     (not, Logical NOT operator)
+
+
+                #######assignment operators
+# x = 5 (assign)
+# x += 5  -----> x=x+5  (add and assign)
+#x -=5 -------> x=x-5   (substract and assign)
+# .. similaryly with other arithmetic opeartors as well 
+
+
+            #######identity operators
+# x  is y (Returns true is same object)
+# x is not y (Return true if not same object)
