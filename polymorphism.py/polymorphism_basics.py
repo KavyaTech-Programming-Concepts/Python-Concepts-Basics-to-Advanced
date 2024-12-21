b3797c8d41cc44a7e769ece6880f734230f2319b
raj <raj@gmail.com>
2024-12-21 11:56:33 +0545

    polymorphism

---
+++ polymorphism.py/polymorphism_basics.py
@@ -0,0 +1,9 @@
+            ##polymorphism , poly + morphism = many form
+##Therefore, in polymorphism we use the same entities like functions, methods, and even operators to behave differently according to the given context.
+#It makes the code easy to use, flexible and extensible.
+
+#The best example is len() function:
+print(len("Hello"))  # 5 (length of a string) 
+print(len([1, 2, 3]))  # 3 (length of a list)
+# Here, at first example, len() function is used to print the length of the given string, ‘hello’
+# But , in the second example, len() function is used to print the length of list
